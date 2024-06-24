8fa49cb33f1acb84a39536a27088a4ce195213fd
guiazca <guiazca@gmail.com>
2024-06-24 19:59:42 +0100

    first commit

---
+++ Models/Dispositivo.cs
@@ -0,0 +1,18 @@
+namespace DevicesApi.Models
+{
+    public class Dispositivo
+    {
+        public int Id { get; set; }
+        public int ModeloId { get; set; }
+        public int LocalizacaoId { get; set; }
+        public string? Nome { get; set; }
+        public string IP { get; set; }
+        public int? Porta { get; set; }
+        public string URL { get; set; }
+        public string? MacAddress { get; set; }
+        public string? Descricao { get; set; }
+
+        public Modelo Modelo { get; set; }
+        public Localizacao Localizacao { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using DevicesApi.Data;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Adicionando serviços ao contêiner.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Devices API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Devices API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseRouting();
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
